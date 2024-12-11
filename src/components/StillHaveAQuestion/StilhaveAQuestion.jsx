82ed2ee01c6687dc5f167e06dd5a9af201cd7980
hocineMK03 <hocine182003@gmail.com>
2024-12-11 12:40:37 +0100

    added components

---
+++ src/components/StillHaveAQuestion/StilhaveAQuestion.jsx
@@ -0,0 +1,47 @@
+import './StiHaveAQuestion.css'
+
+
+import instgramLogo from '../../assets/Instagram.png'
+import facebookLogo from '../../assets/Facebook.png'
+import linkedinLogo from '../../assets/Linkedin.png'
+import './StiHaveAQuestion.css'
+const StilhaveAQuestion = () => {
+  return (
+    <section className='still-have-a-question'>
+        <div className='still-have-a-question-content'>
+            <h1>Still have a question?</h1>
+            <p id='contentquestion'>Contact Us</p>
+
+            <div className='still-have-a-question-cards'>
+                <div className='still-have-a-question-card'>
+                <div className='still-have-a-question-card-icon'>
+                    <img src={instgramLogo} alt='instagram' />
+                    </div>
+                    <p>Instagram</p>
+                </div>
+
+                <div className='still-have-a-question-card'>
+                <div className='still-have-a-question-card-icon'>
+                    <span>
+                    <img src={facebookLogo} alt='instagram' />
+                    </span>
+                    </div>
+                    <p>Facebook</p>
+                </div>
+
+                <div className='still-have-a-question-card'>
+                    <div className='still-have-a-question-card-icon'>
+                    <span>
+                    <img src={linkedinLogo} alt='instagram' />
+                    </span>
+                    </div>
+                    <p>Linkedin</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default StilhaveAQuestion
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,17 @@
+import CountDown from '../../components/CountDown/CountDown';
 import Navbar from '../../components/Navbar/Navbar';
+import StilhaveAQuestion from '../../components/StillHaveAQuestion/StilhaveAQuestion';
+import WhyParticipate from '../../components/WhyParticipate/WhyParticipate';
 import './Landing.css';
 const Landing = () => {
   return (
     <div>
       <Navbar />
+
+      <WhyParticipate />
+
+      <StilhaveAQuestion />
+      <CountDown />
     </div>
   );
 };
