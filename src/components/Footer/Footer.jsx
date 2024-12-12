22aaa6eb718968334ec865845ea4e010e35ee50e
hocineMK03 <hocine182003@gmail.com>
2024-12-12 22:50:11 +0100

    added components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,46 @@
 import './Footer.css';
+
+import { FaLocationDot } from "react-icons/fa6";
+
+import { GrInstagram } from "react-icons/gr";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import bdcLogo from '../../assets/BSC.svg'
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='footer'>
+      <div className='footer-content'>
+        <div className='footer-content-top'>
+          <div className='footer-content-top-desc'>
+
+            <img src={bdcLogo} id='bdclogo' />
+            <button>Apply Now</button>
+          </div>
+          <div className='footer-content-top-hrefs'>
+            <a href='#'>Home</a>
+            <a href='#'>Why</a>
+            <a href='#'>Agenda</a>
+            <a href='#'>Schedule</a>
+            <a href='#'>Sponsor</a>
+            <a href='#'>FAQ</a>
+          </div>
+          <div className='footer-content-top-location'>
+            <i className='location-icon'><FaLocationDot /></i>
+            <a href='#'>Auberge corso boumerdes</a>
+          </div>
+        </div>
+        <div className='footer-content-line'></div>
+        <div className='footer-content-bottom'>
+          <p>© 2024 DevUp. All Rights Reserved.</p>
+          <div className='footer-content-bottom-social'>
+          <i className='social-icon'><GrInstagram /></i>
+          <i className='social-icon'><FaLinkedin /></i>
+          <i className='social-icon'><FaFacebook /></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
