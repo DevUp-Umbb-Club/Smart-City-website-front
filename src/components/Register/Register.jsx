0d74ceab4b8352715171e800bf01401650fcfe7f
Ghalem Salim <salimghalem40@gmail.com>
2024-12-12 20:36:51 +0100

    register done and finished

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, CircularProgress, Snackbar } from '@mui/material';
 import axios from 'axios';
 import './register.css';
-import { useState, useEffect } from 'react';
 import StepOne from '../Steps/StepOne/StepOne';
 import StepTwo from '../Steps/StepTwo/StepTwo';
 import StepThree from '../Steps/StepThree/StepThree';
 import StepFour from '../Steps/StepFour/StepFour';
+import RegisterSuccess from '../RegisterSuccess/RegisterSuccess';
 
 const ApiUrl = import.meta.env.VITE_API_URL;
 
 export default function Register({ open, close }) {
   const [step, setStep] = useState(1);
   const [errorMessage, setErrorMessage] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isExploding, setIsExploding] = useState(false);
   const [progressBarWidth, setprogressBarWidth] = useState(0);
   const [formData, setFormData] = useState({
     firstName: '',
@@ -33,27 +37,25 @@ export default function Register({ open, close }) {
   });
 
   const handleSubmit = async () => {
-    console.log(formData); // You can keep this to see the data in the console
-
+    setLoading(true);
     try {
       const response = await axios.post(`${ApiUrl}`, formData, {
         headers: {
-          'Content-Type': 'application/json', // Ensure correct content type for JSON
+          'Content-Type': 'application/json',
         },
       });
 
       if (response.status === 201) {
-        console.log('Form data submitted successfully');
-        // Optionally, you can handle a success message or redirect here
+        setSuccess(true);
+        setIsExploding(true);
       } else {
         setErrorMessage('An error occurred during the submission');
       }
     } catch (error) {
-      console.error(error);
-      setErrorMessage('An error occurred during the submission');
+      setErrorMessage(`${error.response.data.error}`);
+    } finally {
+      setLoading(false);
     }
-
-    close(); // Close the modal after submission
   };
 
   const validateForm = (step) => {
@@ -126,6 +128,75 @@ export default function Register({ open, close }) {
   return (
     <Modal open={open} onClose={close} aria-labelledby="registration-title">
       <Box className="modal-container">
+        {loading ? (
+          <div
+            style={{
+              minHeight: '650px',
+              display: 'grid',
+              placeItems: 'center',
+            }}
+          >
+            <CircularProgress />
+          </div>
+        ) : (
+          <>
+            {success ? (
+              <RegisterSuccess close={close} isExploding={isExploding} />
+            ) : (
+              <>
+                <div className="progress-bar">
+                  <div
+                    className={`progress-bar-fill active`}
+                    style={{ width: `${progressBarWidth}%` }}
+                  ></div>
+
+                  <div className={`progress-step ${step >= 1 && 'active'}`}>
+                    <div className="step-label">Personal</div>
+                    {step >= 1 && <div className="step-triangle"></div>}
+                    <div className="step-number">01</div>
+                  </div>
+                  <div className={`progress-step ${step >= 2 && 'active'}`}>
+                    <div className="step-label">Skills</div>
+                    {step >= 2 && <div className="step-triangle"></div>}
+
+                    <div className="step-number">02</div>
+                  </div>
+                  <div className={`progress-step ${step >= 3 && 'active'}`}>
+                    <div className="step-label">Professional</div>
+                    {step >= 3 && <div className="step-triangle"></div>}
+
+                    <div className="step-number">03</div>
+                  </div>
+                  <div className={`progress-step ${step >= 4 && 'active'}`}>
+                    <div className="step-label">Team</div>
+                    {step >= 4 && <div className="step-triangle"></div>}
+
+                    <div className="step-number">04</div>
+                  </div>
+                </div>
+                <h1 className="title">Registration</h1>
+
+                {returnStep()}
+                <div className="button-group">
+                  {step !== 1 ? (
+                    <Button
+                      className="back-button"
+                      onClick={handleBack}
+                      disabled={step === 1}
+                    >
+                      Back
+                    </Button>
+                  ) : (
+                    <div></div>
+                  )}
+                  <Button className="next-button" onClick={handleNext}>
+                    {step !== 4 ? 'Next' : 'Register'}
+                  </Button>
+                </div>
+              </>
+            )}
+          </>
+        )}
         {errorMessage && (
           <Snackbar
             open={true}
@@ -142,55 +213,6 @@ export default function Register({ open, close }) {
             </Alert>
           </Snackbar>
         )}
-
-        <div className="progress-bar">
-          <div
-            className={`progress-bar-fill active`}
-            style={{ width: `${progressBarWidth}%` }}
-          ></div>
-
-          <div className={`progress-step ${step >= 1 && 'active'}`}>
-            <div className="step-label">Personal</div>
-            {step >= 1 && <div className="step-triangle"></div>}
-            <div className="step-number">01</div>
-          </div>
-          <div className={`progress-step ${step >= 2 && 'active'}`}>
-            <div className="step-label">Skills</div>
-            {step >= 2 && <div className="step-triangle"></div>}
-
-            <div className="step-number">02</div>
-          </div>
-          <div className={`progress-step ${step >= 3 && 'active'}`}>
-            <div className="step-label">Professional</div>
-            {step >= 3 && <div className="step-triangle"></div>}
-
-            <div className="step-number">03</div>
-          </div>
-          <div className={`progress-step ${step >= 4 && 'active'}`}>
-            <div className="step-label">Team</div>
-            {step >= 4 && <div className="step-triangle"></div>}
-
-            <div className="step-number">04</div>
-          </div>
-        </div>
-        <h1 className="title">Registration</h1>
-        {returnStep()}
-        <div className="button-group">
-          {step !== 1 ? (
-            <Button
-              className="back-button"
-              onClick={handleBack}
-              disabled={step === 1}
-            >
-              Back
-            </Button>
-          ) : (
-            <div></div>
-          )}
-          <Button className="next-button" onClick={handleNext}>
-            {step !== 4 ? 'Next' : 'Register'}
-          </Button>
-        </div>
       </Box>
     </Modal>
   );
