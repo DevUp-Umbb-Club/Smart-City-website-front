a1d199fbc74320ace0a0b1eadc24bea81be8a363
Ghalem Salim <salimghalem40@gmail.com>
2024-12-12 14:28:10 +0100

    sponsors done

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -1,6 +1,14 @@
 import './Sponsor.css';
 const Sponsor = () => {
-  return <div>Sponsor</div>;
+  return (
+    <section className="sponsor-section">
+      <h2>Sponsor</h2>
+      <div className="sponsor-card">
+        <img src="https://placehold.co/600x400.png" alt="Sponsor" />
+        <p>Direction de la Jeunesse et des Sports wilaya de Boumerdes.</p>
+      </div>
+    </section>
+  );
 };
 
 export default Sponsor;
