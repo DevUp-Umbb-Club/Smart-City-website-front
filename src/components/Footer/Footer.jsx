5efd4f098f2c3e7f9799c5bb1621955423bb8a48
hocineMK03 <hocine182003@gmail.com>
2024-12-12 23:16:52 +0100

    added components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,18 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import bdcLogo from '../../assets/BSC.svg'
 const Footer = () => {
+
+  const handleLink=(goto)=>{
+    if(goto==='instagram'){
+      window.location.href='https://www.instagram.com/devup.umbb/'
+    }
+    else if(goto==='facebook'){
+      window.location.href='https://www.facebook.com/devup.umbb'
+    }
+    else if(goto==='linkedin'){
+      window.location.href='https://www.linkedin.com/company/devupumbb'
+    }
+  }
   return (
     <footer className='footer'>
       <div className='footer-content'>
@@ -33,9 +45,15 @@ const Footer = () => {
         <div className='footer-content-bottom'>
           <p>© 2024 DevUp. All Rights Reserved.</p>
           <div className='footer-content-bottom-social'>
-          <i className='social-icon'><GrInstagram /></i>
-          <i className='social-icon'><FaLinkedin /></i>
-          <i className='social-icon'><FaFacebook /></i>
+          <i className='social-icon' onClick={()=>{
+            handleLink('instagram')
+          }}><GrInstagram /></i>
+          <i className='social-icon' onClick={()=>{
+            handleLink('facebook')
+          }}><FaLinkedin /></i>
+          <i className='social-icon' onClick={()=>{
+            handleLink('linkedin')
+          }}><FaFacebook /></i>
           </div>
         </div>
       </div>
